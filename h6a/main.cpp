657cd25adde6d17ca323eb9e1dcf9a7dedb165ce
unknown <nlindqvu24@students.oamk.fi>
2025-12-14 17:34:49 +0200

    h6a valmis

---
+++ h6a/main.cpp
@@ -0,0 +1,9 @@
+#include "myclass.h"
+
+int main()
+{
+    MyClass obj;        // luodaan olio luokasta MyClass
+    obj.raiseMySignal(); // kutsutaan metodia, joka emitoi signaalin
+
+    return 0;
+}
